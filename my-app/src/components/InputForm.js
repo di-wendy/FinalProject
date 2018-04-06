0cc5f9870f6bce56b86dd5bd48fcf3ec1d4ff56f
dwang3 <di_wang@intuit.com>
2018-04-05 22:29:09 -0700

    bug-free version

--- my-app/src/components/InputForm.js
+++ my-app/src/components/InputForm.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import { connect } from 'react-redux';
 
 export class InputForm extends React.Component {
 
@@ -13,52 +12,69 @@ export class InputForm extends React.Component {
 
     render(){
         return(<form className="form">
-                <div className="form-group col-md-6">
-                    <label htmlFor="name" >Name</label>
-                        <input className='nameInput' type="text" id="name" name="nameInput" ref={this.nameRef}/>
-                <div className="form-group col-md-6">
-                    <label htmlFor="message">Message</label>
-                        <input type="text" id="message" name="messageInput" ref={this.messageRef}/>
-                </div>
-                <div className="form-group col-md-6">
-                <label htmlFor="image">Image</label>
-                    <input type="text" id="image" name="imageInput" ref={this.imageRef}/>
-                </div>
-                <button className="btn btn-primary btn-lg" type="button" onClick={()=> {
-                    if(this.nameRef.current.value.length === 0){
-                        alert("Name must be at least 1 character")
-                    }
-                    else{
-                        if (this.imageRef.current.value.length >0){
-                            if(this.imageRef.current.value.startsWith('http://') && 
-                            this.imageRef.current.value.endsWith('.jpg')){
-                                this.props.chat.send({
-                                    name : this.nameRef.current.value.value,
-                                    message : this.messageRef.current.value,
-                                    image : this.imageRef.current.value
-                                }).then(({id}) => {
-                                    //console.log(id)
-                                }).catch(e => {
-                                    console.log(e)
-                                })
-                            }
-                            else{
-                                alert("Image must be a URL");
-                            }
+                    <div className="form-group col-md-6">
+                        <label htmlFor="name" >Name</label>
+                            <input className='nameInput' type="text" id="name" name="nameInput" ref={this.nameRef}/>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="message">Message</label>
+                            <input type="text" id="message" name="messageInput" ref={this.messageRef}/>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="image">ImageURL</label>
+                            <input type="text" id="image" name="imageInput" ref={this.imageRef}/>
+                    </div>
+                    <button className="btn btn-primary btn-lg" type="button" onClick={()=> {
+                        if(this.nameRef.current.value.length === 0){
+                            alert("Name must be at least 1 character")
                         }
                         else{
-                            this.props.chat.send({
-                                name : this.nameRef.current.value,
-                                message : this.messageRef.current.value
-                            }).then(({id}) => {
-                                //console.log(id)
-                            }).catch(e => {
-                                console.log(e)
-                            })
+                            if (this.imageRef.current.value.length > 0){
+                                if(this.imageRef.current.value.startsWith('http'))
+                                {
+                                    if(this.messageRef.current.value.length===0){
+                                        this.props.chat.send({
+                                            name : this.nameRef.current.value,
+                                            image : this.imageRef.current.value
+                                        }).then(({id}) => {
+                                            //console.log(id)
+                                        }).catch(e => {
+                                            console.log(e)
+                                        })
+                                    }
+                                    else{
+                                        this.props.chat.send({
+                                            name : this.nameRef.current.value,
+                                            message : this.messageRef.current.value,
+                                            image : this.imageRef.current.value
+                                        }).then(({id}) => {
+                                            //console.log(id)
+                                        }).catch(e => {
+                                            console.log(e)
+                                        })
+                                    }
+                                }
+                                else{
+                                    alert("Image must be a valid URL");
+                                }
+                            }
+                            else {
+                                if(this.messageRef.current.value.length === 0){
+                                    alert("Please enter message or image url")
+                                }
+                                else{
+                                    this.props.chat.send({
+                                        name : this.nameRef.current.value,
+                                        message : this.messageRef.current.value
+                                    }).then(({id}) => {
+                                        //console.log(id)
+                                    }).catch(e => {
+                                        console.log(e)
+                                    })
+                                }
+                            }
                         }
-                    }
-                }}>Send</button>
-            </div>
+                    }}>Send</button>
+                </div>
             </form>
         )
     }
